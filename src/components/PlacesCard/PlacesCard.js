0fee9a2b3380f638a0b0f729f8690517494931dd
Sara Karsh <saralkarsh@gmail.com>
2019-11-02 15:59:21 -0600

    Add landscape icons

--- src/components/PlacesCard/PlacesCard.js
+++ src/components/PlacesCard/PlacesCard.js
@@ -1,5 +1,27 @@
 import React from 'react'
 import './PlacesCard.scss'
+import mountain from '../../images/mountains.png'
+import hill from '../../images/hills.png'
+import plain from '../../images/field.png'
+import marsh from '../../images/marsh.png'
+import forest from '../../images/trees.png'
+import city from '../../images/cityscape.png'
+import river from '../../images/waterfall.png'
+import ocean from '../../images/cyclone.png'
+
+
+
+const placeIcons = {
+  Mountain: mountain,
+  Hill: hill,
+  Plain: plain,
+  Marsh: marsh,
+  Hill: hill,
+  Forest: forest,
+  City: city,
+  River: river,
+  Ocean: ocean
+}
 
 export const PlacesCard = ({ climate, name, terrain }) => {
   return (
@@ -8,6 +30,7 @@ export const PlacesCard = ({ climate, name, terrain }) => {
       <div className='place-details'>
         <h4>Climate: {climate}</h4>
         <h4>Terrain: {terrain}</h4>
+        <img src={placeIcons[terrain]} />
       </div>
     </section>
   )
