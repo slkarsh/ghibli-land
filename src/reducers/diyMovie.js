53bcf7fcf13481c1b5dc384ac9c9305de79c40cc
Sara Karsh <saralkarsh@gmail.com>
2019-11-02 18:50:59 -0600

    Remove unnecessary actions

--- src/reducers/diyMovie.js
+++ src/reducers/diyMovie.js
@@ -8,37 +8,6 @@ const initialState = {
 
 export const diyMovie = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_CHARACTER':
-      return {
-        ...state,
-        characters: [...state.characters, action.character]
-      }
-    case 'REMOVE_CHARACTER': {
-      const newCharacterState = state.characters.filter(character => {
-        return character.name !== action.character.name
-      })
-      return newCharacterState
-    }
-    case 'ADD_LOCATION':
-      return {
-        ...state,
-        locations: [...state.locations, action.location]
-      }
-    case 'REMOVE_LOCATION': 
-      const newLocationState = state.locations.filter(location => {
-        return location.name !== action.location.name
-      })
-      return newLocationState
-    case 'ADD_VEHICLE':
-      return {
-        ...state,
-        vehicles: [...state.vehicles,action.vehicle]
-      }
-    case 'REMOVE_VEHICLE':
-      const newVehicleState = state.vehicles.filter(vehicle => {
-        return vehicle.name !== action.vehicle.name
-      })
-      return newVehicleState
     case 'TOGGLE_ADD_CHARACTER': 
       const existingCharacter = state.characters.find(character => {
         return character.name === action.character.name
@@ -97,10 +66,3 @@ export const diyMovie = (state = initialState, action) => {
       return state
   }
 }
-
-
-
-// const newState = state.filter(favorite => {
-//   return favorite.movie_id !== action.movieId
-// })
-// return newState;
