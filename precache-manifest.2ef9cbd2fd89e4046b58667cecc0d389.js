f0dcf46013aaef133c82f0230c10c62157d7a02d
Sara <saralkarsh@gmail.com>
2020-01-06 20:49:24 -0700

    Updates

---
+++ precache-manifest.2ef9cbd2fd89e4046b58667cecc0d389.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a79249a7c441925b4d4af680a5fd91af",
+    "url": "/ghibli-land/index.html"
+  },
+  {
+    "revision": "80e19eccb50065ed6dd8",
+    "url": "/ghibli-land/static/css/main.cf2c8ab8.chunk.css"
+  },
+  {
+    "revision": "fbfeb02889e3e889ee4f",
+    "url": "/ghibli-land/static/js/2.d20cd28c.chunk.js"
+  },
+  {
+    "revision": "80e19eccb50065ed6dd8",
+    "url": "/ghibli-land/static/js/main.792eb24d.chunk.js"
+  },
+  {
+    "revision": "a603cde726d0f37f8c88",
+    "url": "/ghibli-land/static/js/runtime-main.b14825dd.js"
+  }
+]);
